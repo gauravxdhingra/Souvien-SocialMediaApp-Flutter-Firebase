32ceaa73ebfa7eca222174eb1b164fc03b8c434b
gauravxdhingra <grvdhingra1999@gmail.com>
2020-05-11 23:08:00 +0530

    Built Search Screen

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -6,9 +7,58 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  AppBar buildSearchField() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      title: TextFormField(
+        decoration: InputDecoration(
+          hintText: 'Search For Users',
+          filled: true,
+          prefixIcon: Icon(
+            Icons.account_box,
+            size: 28,
+          ),
+          suffixIcon: IconButton(
+            icon: Icon(Icons.clear),
+            onPressed: () => print('cleared'),
+          ),
+        ),
+      ),
+    );
+  }
+
+  buildNoContent() {
+    return Container(
+      child: Center(
+        child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            SvgPicture.asset(
+              'assets/images/search.svg',
+             ),
+            // Text(
+            //   'Find Users',
+            //   textAlign: TextAlign.center,
+            //   style: TextStyle(
+            //     color: Colors.white,
+            //     fontStyle: FontStyle.italic,
+            //     fontWeight: FontWeight.w600,
+            //     fontSize: 60,
+            //   ),
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Search');
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor.withOpacity(0.7),
+      appBar: buildSearchField(),
+      body: buildNoContent(),
+    );
   }
 }
 
