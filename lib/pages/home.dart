c4cb748cf37a176d1a797aa2ec3558204d9a0648
gauravxdhingra <grvdhingra1999@gmail.com>
2020-05-16 01:50:28 +0530

    Built Timeline

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -23,6 +23,7 @@ final commentsRef = Firestore.instance.collection('comments');
 final activityFeedRef = Firestore.instance.collection('feed');
 final followersRef = Firestore.instance.collection('followers');
 final followingRef = Firestore.instance.collection('following');
+final timelineRef = Firestore.instance.collection('timeline');
 
 final DateTime timestamp = DateTime.now();
 User currentUser;
@@ -123,7 +124,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: PageView(
         children: <Widget>[
-          Timeline(),
+          Timeline(currentUser: currentUser),
           // Container(
           //   height: 50,
           //   width: 200,
--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,18 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttershare/models/user.dart';
 import 'package:fluttershare/widgets/header.dart';
+import 'package:fluttershare/widgets/post.dart';
 import '../widgets/progress.dart';
+import 'home.dart';
 
 // final usersRef = Firestore.instance.collection('users');
 final usersRef = Firestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
+  final User currentUser;
+
+  const Timeline({Key key, this.currentUser}) : super(key: key);
+
   @override
   _TimelineState createState() => _TimelineState();
 }
 
 class _TimelineState extends State<Timeline> {
-  List<dynamic> users = [];
+  // List<dynamic> users = [];
+  List<Post> posts;
 
   @override
   void initState() {
@@ -21,7 +29,22 @@ class _TimelineState extends State<Timeline> {
     // createUser();
     // updateUser();
     // deleteUser();
+    getTimeline();
     super.initState();
+  }
+
+  getTimeline() async {
+    QuerySnapshot snapshot = await timelineRef
+        .document(widget.currentUser.id)
+        .collection('timelinePosts')
+        .orderBy('timestamp', descending: true)
+        .getDocuments();
+
+    List<Post> posts =
+        snapshot.documents.map((doc) => Post.fromDocument(doc)).toList();
+    setState(() {
+      this.posts = posts;
+    });
   }
 
   // createUser() async {
@@ -81,11 +104,25 @@ class _TimelineState extends State<Timeline> {
   //   });
   // }
 
+  buildTimeline() {
+    if (posts == null) {
+      return circularProgress();
+    } else if (posts.isEmpty) {
+      return Text('No Posts Yet! Start Following People :-)');
+    } else
+      return ListView(
+        children: posts,
+      );
+  }
+
   @override
   Widget build(context) {
     return Scaffold(
       appBar: header(context, isAppTitle: true),
-      body: Text('Timeline'),
+      body: RefreshIndicator(
+        onRefresh: () => getTimeline(),
+        child: buildTimeline(),
+      ),
       //  Container(
       //   child: ListView(
       //     children: users.map((user) => Text(user['username'])).toList(),
